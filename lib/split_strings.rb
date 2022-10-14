4ad9ccbcbd146045daa23784ec24426bf4a79c58
Bas Maw <sebastien.maw@madetech.com>
2022-10-14 17:06:41 +0100

    test string returns string

--- lib/split_strings.rb
+++ lib/split_strings.rb
@@ -1,2 +1,5 @@
 # Ruby program to split a string and return a list of each 2 strings
 
+def get_the_string(string)
+    string
+end
---
+++ spec/check_strings_spec.rb
@@ -0,0 +1,7 @@
+require 'split_strings'
+
+describe 'initial test' do
+    it 'string equals string' do
+        expect(get_the_string("thisisastring")).to eq("thisisastring")
+    end
+end
